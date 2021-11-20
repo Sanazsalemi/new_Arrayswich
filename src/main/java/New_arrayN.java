a71917b66e26229067dbad40015903c904b1072a
sanaz salemi <sanazsalemi9898>
2021-11-20 15:27:39 -0800

    Initial commit

---
+++ src/main/java/New_arrayN.java
@@ -0,0 +1,39 @@
+
+public class New_arrayN {
+    public static void main(String[] args) {
+        int[] myArray = {1, 4, 7, 9};
+        String[] Str = {"One", "FOur", "Seven", "Nine"};
+    }
+    private void intToString(int[] inputArray) {
+        int inputArrayLength = inputArray.length;
+        String[] ouputeArray = new String[inputArrayLength];
+
+
+        for (int i = 0; i < inputArray.length; i++) {
+            if (inputArray[i] == 1) {
+                System.out.println(ouputeArray[i]);
+            } else if (inputArray[i] == 4) {
+                System.out.println(ouputeArray[i]);
+            } else if (inputArray[i] == 7) {
+                System.out.println(ouputeArray[i]);
+            } else if (inputArray[i] == 9) {
+                System.out.println(ouputeArray[i]);
+            } else {
+                System.out.println("null");
+            }
+
+        }
+        for (String String1 : ouputeArray) {
+            System.out.println(String1 + "");}
+            System.out.println(ouputeArray);
+        }
+
+    }
+
+
+
+
+
+
+
+
